cd279d9a7267551fbdb81012f98f073becb11ab1
Kovács Sámuel <sunikovacs@gmail.com>
2022-02-05 15:35:48 +0100

    add http part

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,65 @@
 #include "SdFat.h"
 #include "sdios.h"
 
+
+
+// Select your modem:
+#define TINY_GSM_MODEM_SIM800
+
+#include <TinyGsmClient.h>
+#include <ArduinoHttpClient.h>
+
+// Set serial for debug console (to the Serial Monitor, default speed 115200)
+#define SerialMon Serial
+
+// Set serial for AT commands (to the module)
+HardwareSerial Serial1(PG9, PG14);
+#define SerialAT Serial1
+
+// Increase RX buffer to capture the entire response
+// Chips without internal buffering (A6/A7, ESP8266, M590)
+// need enough space in the buffer for the entire response
+// else data will be lost (and the http library will fail).
+#if !defined(TINY_GSM_RX_BUFFER)
+#define TINY_GSM_RX_BUFFER 1024
+#endif
+
+// Define the serial console for debug prints, if needed
+#define TINY_GSM_DEBUG SerialMon
+// #define LOGGING  // <- Logging is for the HTTP library
+
+// Range to attempt to autobaud
+// NOTE:  DO NOT AUTOBAUD in production code.  Once you've established
+// communication, set a fixed baud rate using modem.setBaud(#).
+#define GSM_AUTOBAUD_MIN 9600
+#define GSM_AUTOBAUD_MAX 115200
+
+// Add a reception delay, if needed.
+// This may be needed for a fast processor at a slow baud rate.
+// #define TINY_GSM_YIELD() { delay(2); }
+
+// set GSM PIN, if any
+#define GSM_PIN "9526"
+
+// Your GPRS credentials, if any
+const char apn[]      = "internet.vodafone.net";
+const char gprsUser[] = "";
+const char gprsPass[] = "";
+
+// Server details
+const char server[]   = "water-minilab.herokuapp.com";  //nem lehet előtte http://
+const int  port       = 80;
+
+TinyGsm        modem(SerialAT);
+TinyGsmClient client(modem);
+HttpClient    http(client, server, port);
+
+#define MODEM_PWRKEY PD15
+
+/******************************************************************************/
+/******************************************************************************/
+
+
 #define ACTIVE_LED BLUE_LED
 
 
@@ -162,6 +221,134 @@ void setup()
   // stepper3.setMaxSpeed(3000.0);
   // stepper3.setAcceleration(3000.0);
   // stepper3.moveTo(100);
+
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  Serial.println("- testing GSM HTTP GET and POST");
+
+  
+  // !!!!!!!!!!!
+  // Set your reset, enable, power pins here
+  // !!!!!!!!!!!
+  pinMode(MCU_GSM_PWRKEY, OUTPUT);
+
+  digitalWrite(MCU_GSM_PWRKEY, HIGH);
+  delay(1000); //Need delay
+  digitalWrite(MCU_GSM_PWRKEY, LOW);
+
+  SerialMon.println("Wait...");
+
+  // Set GSM module baud rate
+  TinyGsmAutoBaud(SerialAT, GSM_AUTOBAUD_MIN, GSM_AUTOBAUD_MAX);
+  // SerialAT.begin(9600);
+  delay(6000);
+
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  SerialMon.println("Initializing modem...");
+  modem.restart();
+  // modem.init();
+
+  String modemInfo = modem.getModemInfo();
+  SerialMon.print("Modem Info: ");
+  SerialMon.println(modemInfo);
+
+  // Unlock your SIM card with a PIN if needed
+  if (GSM_PIN && modem.getSimStatus() != 3) { modem.simUnlock(GSM_PIN); }
+
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  SerialMon.print("Waiting for network...");
+  if (!modem.waitForNetwork()) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println(" success");
+
+  if (modem.isNetworkConnected()) { SerialMon.println("Network connected"); }
+
+  // GPRS connection parameters are usually set after network registration
+  SerialMon.print(F("Connecting to "));
+  SerialMon.print(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println(" success");
+
+  if (modem.isGprsConnected()) { SerialMon.println("GPRS connected"); }
+
+  SerialMon.print(F("Performing HTTP GET request... "));
+  int err = http.get("/binarysize");
+  if (err != 0) {
+    SerialMon.println(F("failed to connect"));
+    delay(10000);
+    return;
+  }
+
+  int status = http.responseStatusCode();
+  SerialMon.print(F("Response status code: "));
+  SerialMon.println(status);
+  if (!status) {
+    delay(10000);
+    return;
+  }
+
+  SerialMon.println(F("Response Headers:"));
+  while (http.headerAvailable()) {
+    String headerName  = http.readHeaderName();
+    String headerValue = http.readHeaderValue();
+    SerialMon.println("    " + headerName + " : " + headerValue);
+  }
+
+  int length = http.contentLength();
+  if (length >= 0) {
+    SerialMon.print(F("Content length is: "));
+    SerialMon.println(length);
+  }
+  if (http.isResponseChunked()) {
+    SerialMon.println(F("The response is chunked"));
+  }
+
+  String body = http.responseBody();
+  SerialMon.println(F("Response:"));
+  SerialMon.println(body);
+
+  SerialMon.print(F("Body length is: "));
+  SerialMon.println(body.length());
+
+  /*********************************************************************/
+  // HTTP POST request
+  SerialMon.print(F("\r\nPerforming HTTP POST request... "));
+
+  http.connectionKeepAlive();
+  http.beginRequest();
+  int codePost = http.post("/upload");
+
+  http.sendHeader(F("Content-Type"), F("application/json"));
+  http.sendHeader(F("Content-Length"), 13);
+  http.beginBody();
+  http.println("{\"num\": \"15\"}");
+  
+  http.endRequest();
+
+  status = http.responseStatusCode();
+  //int status = http.responseStatusCode();
+  SerialMon.print(F("Response status code: "));
+  SerialMon.println(status);
+  /*********************************************************************/
+
+  // Shutdown
+  http.stop();
+  SerialMon.println(F("Server disconnected"));
+
+  modem.gprsDisconnect();
+  SerialMon.println(F("GPRS disconnected"));
+
+  // Do nothing forevermore
+  while (true) { delay(1000); }
 }
 
 
@@ -187,15 +374,10 @@ void loop()
   // dc_pump_2.stop();
 
   // Change direction at the limits
-  if (stepper1.distanceToGo() == 0)
-  {
-    stepper1.moveTo(-stepper1.currentPosition());
-  }
-  stepper1.run();
-
-  // if (stepper3.distanceToGo() == 0)
+  // if (stepper1.distanceToGo() == 0)
   // {
-  //   stepper3.moveTo(-stepper3.currentPosition());
+  //   stepper1.moveTo(-stepper1.currentPosition());
   // }
-  // stepper3.run();
+  // stepper1.run();
+
 }
