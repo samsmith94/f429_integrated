cc3bc9387caff026b15d9f1c34e23d27032ac28c
Kovács Sámuel <sunikovacs@gmail.com>
2022-02-05 17:55:51 +0100

    add checksum script

---
+++ checksum.bat
@@ -0,0 +1,23 @@
+rem fill gaps
+srec_cat .pio\build\nucleo_f429zi\firmware.hex ^
+-Intel ^
+-fill 0xFF 0x08080000 0x080E0000 ^
+-o filled.hex ^
+-Intel ^
+-line-length=44
+
+rem sign
+srec_cat filled.hex ^
+-Intel ^
+-crop 0x08080000 0x080DFFFC ^
+-STM32 0x080DFFFC ^
+-o signed_app.hex ^
+-Intel
+
+rem hex to binary
+arm-none-eabi-objcopy --input-target=ihex --output-target=binary signed_app.hex signed_app.bin
+
+del filled.hex
+del signed_app.hex
+
+@REM pause
