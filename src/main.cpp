f4f97cf73eea909955ba0d2da4342b8593c1314b
Kovács Sámuel <sunikovacs@gmail.com>
2022-02-05 17:34:55 +0100

    incredible: zip download works besides get&post

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,12 @@
 #include "SdFat.h"
 #include "sdios.h"
 
-
-
 // Select your modem:
 #define TINY_GSM_MODEM_SIM800
 
 #include <TinyGsmClient.h>
 #include <ArduinoHttpClient.h>
+#include <CRC32.h>                //unzipLIB-el probléma lesz!!!
 
 // Set serial for debug console (to the Serial Monitor, default speed 115200)
 #define SerialMon Serial
@@ -63,6 +62,11 @@ const char gprsPass[] = "";
 // Server details
 const char server[]   = "water-minilab.herokuapp.com";  //nem lehet előtte http://
 const int  port       = 80;
+
+const char zip_resource[]    = "/download";
+uint32_t   knownCRC32    = 0xa26a7a0a;
+uint32_t   knownFileSize = 22820 ;  // In case server does not send it
+
 
 TinyGsm        modem(SerialAT);
 TinyGsmClient client(modem);
@@ -168,6 +172,18 @@ void sw8_ISR()
 {
   Serial.println("SW8 interrupt");
 }
+
+void printPercent(uint32_t readLength, uint32_t contentLength) {
+  // If we know the total length
+  if (contentLength != (uint32_t)-1) {
+    SerialMon.print("\r ");
+    SerialMon.print((100.0 * readLength) / contentLength);
+    SerialMon.print('%');
+  } else {
+    SerialMon.println(readLength);
+  }
+}
+
 void setup()
 {
   Serial.begin(115200);
@@ -207,22 +223,23 @@ void setup()
   pinMode(USER_BUTTON, INPUT);
   attachInterrupt(digitalPinToInterrupt(USER_BUTTON), button_ISR, FALLING);
 
-  pinMode(SW1, INPUT);
-  attachInterrupt(digitalPinToInterrupt(SW1), sw1_ISR, FALLING);
-  pinMode(SW2, INPUT);
-  attachInterrupt(digitalPinToInterrupt(SW2), sw2_ISR, FALLING);
-  pinMode(SW3, INPUT);
-  attachInterrupt(digitalPinToInterrupt(SW3), sw3_ISR, FALLING);
-  pinMode(SW4, INPUT);
-  attachInterrupt(digitalPinToInterrupt(SW4), sw4_ISR, FALLING);
-  pinMode(SW5, INPUT);
-  attachInterrupt(digitalPinToInterrupt(SW5), sw5_ISR, FALLING);
-  pinMode(SW6, INPUT);
-  attachInterrupt(digitalPinToInterrupt(SW6), sw6_ISR, FALLING);
-  pinMode(SW7, INPUT);
-  attachInterrupt(digitalPinToInterrupt(SW7), sw7_ISR, FALLING);
-  pinMode(SW8, INPUT);
-  attachInterrupt(digitalPinToInterrupt(SW8), sw8_ISR, FALLING);
+  //sajnos nem igazán szereti ha közben a http megy :D
+  // pinMode(SW1, INPUT);
+  // attachInterrupt(digitalPinToInterrupt(SW1), sw1_ISR, FALLING);
+  // pinMode(SW2, INPUT);
+  // attachInterrupt(digitalPinToInterrupt(SW2), sw2_ISR, FALLING);
+  // pinMode(SW3, INPUT);
+  // attachInterrupt(digitalPinToInterrupt(SW3), sw3_ISR, FALLING);
+  // pinMode(SW4, INPUT);
+  // attachInterrupt(digitalPinToInterrupt(SW4), sw4_ISR, FALLING);
+  // pinMode(SW5, INPUT);
+  // attachInterrupt(digitalPinToInterrupt(SW5), sw5_ISR, FALLING);
+  // pinMode(SW6, INPUT);
+  // attachInterrupt(digitalPinToInterrupt(SW6), sw6_ISR, FALLING);
+  // pinMode(SW7, INPUT);
+  // attachInterrupt(digitalPinToInterrupt(SW7), sw7_ISR, FALLING);
+  // pinMode(SW8, INPUT);
+  // attachInterrupt(digitalPinToInterrupt(SW8), sw8_ISR, FALLING);
 
   Serial.println("- testing DC pumps");
   dc_pump_2.setSpeed(255);
@@ -413,6 +430,153 @@ void setup()
   SerialMon.println(" success");
 
   if (modem.isGprsConnected()) { SerialMon.println("GPRS connected"); }
+
+  /*********************************************************************/
+  // HTTP download zip
+
+  SerialMon.print(F("Connecting to "));
+  SerialMon.print(server);
+  if (!client.connect(server, port)) {
+    SerialMon.println(" fail");
+    delay(10000);
+    return;
+  }
+  SerialMon.println(" success");
+
+  // Make a HTTP GET request:
+  client.print(String("GET ") + zip_resource + " HTTP/1.0\r\n");
+  client.print(String("Host: ") + server + "\r\n");
+  client.print("Connection: close\r\n\r\n");
+
+  // Let's see what the entire elapsed time is, from after we send the request.
+  uint32_t timeElapsed = millis();
+
+  SerialMon.println(F("Waiting for response header"));
+
+  // While we are still looking for the end of the header (i.e. empty line
+  // FOLLOWED by a newline), continue to read data into the buffer, parsing each
+  // line (data FOLLOWED by a newline). If it takes too long to get data from
+  // the client, we need to exit.
+
+  const uint32_t clientReadTimeout   = 5000;
+  uint32_t       clientReadStartTime = millis();
+  String         headerBuffer;
+  bool           finishedHeader = false;
+  uint32_t       contentLength  = 0;
+
+  while (!finishedHeader) {
+    int nlPos;
+
+    if (client.available()) {
+      clientReadStartTime = millis();
+      while (client.available()) {
+        char c = client.read();
+        headerBuffer += c;
+
+        // Uncomment the lines below to see the data coming into the buffer
+        // if (c < 16)
+        //   SerialMon.print('0');
+        // SerialMon.print(c, HEX);
+        // SerialMon.print(' ');
+        // if (isprint(c))
+        //   SerialMon.print(reinterpret_cast<char> c);
+        // else
+        //   SerialMon.print('*');
+        // SerialMon.print(' ');
+
+        // Let's exit and process if we find a new line
+        if (headerBuffer.indexOf(F("\r\n")) >= 0) break;
+      }
+    } else {
+      if (millis() - clientReadStartTime > clientReadTimeout) {
+        // Time-out waiting for data from client
+        SerialMon.println(F(">>> Client Timeout !"));
+        break;
+      }
+    }
+
+    // See if we have a new line.
+    nlPos = headerBuffer.indexOf(F("\r\n"));
+
+    if (nlPos > 0) {
+      headerBuffer.toLowerCase();
+      // Check if line contains content-length
+      if (headerBuffer.startsWith(F("content-length:"))) {
+        contentLength =
+            headerBuffer.substring(headerBuffer.indexOf(':') + 1).toInt();
+        // SerialMon.print(F("Got Content Length: "));  // uncomment for
+        // SerialMon.println(contentLength);            // confirmation
+      }
+
+      headerBuffer.remove(0, nlPos + 2);  // remove the line
+    } else if (nlPos == 0) {
+      // if the new line is empty (i.e. "\r\n" is at the beginning of the line),
+      // we are done with the header.
+      finishedHeader = true;
+    }
+  }
+
+  // The two cases which are not managed properly are as follows:
+  // 1. The client doesn't provide data quickly enough to keep up with this
+  // loop.
+  // 2. If the client data is segmented in the middle of the 'Content-Length: '
+  // header,
+  //    then that header may be missed/damaged.
+  //
+
+  uint32_t readLength = 0;
+  CRC32    crc;
+
+  if (finishedHeader && contentLength == knownFileSize) {
+    SerialMon.println(F("Reading response data"));
+    clientReadStartTime = millis();
+
+    printPercent(readLength, contentLength);
+    while (readLength < contentLength && client.connected() &&
+           millis() - clientReadStartTime < clientReadTimeout) {
+      while (client.available()) {
+        uint8_t c = client.read();
+        // SerialMon.print(reinterpret_cast<char>c);  // Uncomment this to show
+        // data
+        crc.update(c);
+        readLength++;
+        if (readLength % (contentLength / 13) == 0) {
+          printPercent(readLength, contentLength);
+        }
+        clientReadStartTime = millis();
+      }
+    }
+    printPercent(readLength, contentLength);
+  }
+
+  timeElapsed = millis() - timeElapsed;
+  SerialMon.println();
+
+  // Shutdown
+
+  //client.stop();
+  //SerialMon.println(F("Server disconnected"));
+
+  //modem.gprsDisconnect();
+  //SerialMon.println(F("GPRS disconnected"));
+
+  float duration = float(timeElapsed) / 1000;
+
+  SerialMon.println();
+  SerialMon.print("Content-Length: ");
+  SerialMon.println(contentLength);
+  SerialMon.print("Actually read:  ");
+  SerialMon.println(readLength);
+  SerialMon.print("Calc. CRC32:    0x");
+  SerialMon.println(crc.finalize(), HEX);
+  SerialMon.print("Known CRC32:    0x");
+  SerialMon.println(knownCRC32, HEX);
+  SerialMon.print("Duration:       ");
+  SerialMon.print(duration);
+  SerialMon.println("s");
+
+  /*********************************************************************/
+  // HTTP GET request
 
   SerialMon.print(F("Performing HTTP GET request... "));
   int err = http.get("/binarysize");
